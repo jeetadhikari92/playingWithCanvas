dc1cd67ba13b3ef9f582237e3769f3184cd582cf
Jeet Adhikari <39948671+jeetadhikari92@users.noreply.github.com>
2019-01-28 16:56:19 +0100

    Update bubble.js

--- bubbles/bubble.js
+++ bubbles/bubble.js
@@ -5,12 +5,28 @@ canvas.height = window.innerWidth;
 
 const c = canvas.getContext('2d');
 
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+
+window.addEventListener("mousemove", function(event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+window.addEventListener("resize", function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerWidth;
+})
+
 function Circle (x, y, dx, dy, radius, color) {
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = radius;
     this.color = color;
 
     this.draw = function() {
@@ -29,30 +45,33 @@ function Circle (x, y, dx, dy, radius, color) {
         }
         this.x += this.dx;
         this.y += this.dy;
+
+        if(mouse.x - this.x < 50 && mouse.x - this.x > -50 &&
+            mouse.y - this.y < 50 && mouse.y - this.y > -50 ) {
+                if(this.radius < 40){
+                    this.radius += 1;
+                }
+            } else if (this.radius > this.minRadius){
+                this.radius -= 1;
+            }
+
         this.draw();
     }
 }
 
 
-let x = Math.random() * innerWidth;
-let y = Math.random() * innerHeight;
-let dx = (Math.random() - 0.5) * 8;
-let dy = (Math.random() - 0.5) * 8;
-let radius = 100;
+let x, y, dx, dy, radius;
 const circleArr = [];
 
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 800; i++) {
     x = Math.random() * (innerWidth - radius * 2) + radius;
     y = Math.random() * (innerHeight - radius *2) + radius;
-    dx = (Math.random() - 0.5) * 8;
-    dy = (Math.random() - 0.5) * 8;
-    radius = Math.random() * 100;
+    dx = (Math.random() - 0.5) * 5;
+    dy = (Math.random() - 0.5) * 5;
+    radius = Math.random() * 3 + 1;
     let randomColor = "#" + Math.floor(Math.random() * 25542195).toString(16);
     circleArr.push(new Circle(x, y, dx, dy, radius, randomColor));
 }
-
-
-const circle = new Circle(100, 100, 4, 4, 100);
 
 function animate() {
     requestAnimationFrame(animate);
@@ -64,27 +83,3 @@ function animate() {
 }
 
 animate();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
